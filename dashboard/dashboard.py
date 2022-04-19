459ab9ec7557e3b993c64fe1b8305ac769e7cc56
hanen371 <96344546+hanen371@users.noreply.github.com>
2022-04-19 19:58:04 +0200

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,26 +1,47 @@
-# Import modules 
+# Import of modules
 import os
 import numpy as np
 import pandas as pd
+from PIL import Image
 import requests
 import json
 import matplotlib.pyplot as plt
+import shap
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
 import streamlit.components.v1 as components
 import lightgbm as lgb
-import shap
 
-########################################
+# let’s add a bit more descriptive text to our UI
+st.write("""
+# Welcome on this dashboard !
+# Context
+The company "Ready to distribute" wishes to set up a "credit scoring" tool to calculate the \
+probability that a customer will repay his credit, then classify the request as granted or \
+refused credit
+The label is a binary variable, 0 (will repay the loan on time), 1 (will have difficulty repaying \
+the loan)
+# Objectives
+ 1. Create a classification model that will automatically predict the likelihood that a customer \
+  can or cannot repay their loan.
+ 2. Build an interactive dashboard for customer relationship managers to interpret the predictions\
+  made by the model, and improve the customer knowledge of customer relationship managers.
+  
+# How to use it ?
+To predict the score of a specific client, you have to choose the client ID.
+To better understand the score, you can compare some informations of the client versus the values of all the others clients.
+The multiselect bow allows you to chose which features to compare.
+""")
+
 abs_path = os.path.dirname(os.path.realpath(__file__))
-html_header="""
+html_header = """
 <head>
 <title>PHomeCredit</title>
 <meta charset="utf-8">
 <meta name="keywords" content="home credit risk, dashboard, Hanen Ben Brahim">
 <meta name="description" content="Home Credit Risk Dashboard">
-<meta name="author" content="Hanen BEN BRAHIM">
+<meta name="author" content="Hanen Ben Brahim">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <h1 style="font-size:300%; color:#838383; font-family:Georgia"> Home Credit Default Risk Dashboard <br>
@@ -29,9 +50,8 @@ html_header="""
 
 st.set_page_config(page_title="Home Credit Default Risk Dashboard", page_icon="", layout="wide")
 
-
 with st.container():
-  col1, col2, col3, col4 = st.columns([1,26,1,5])
+  col1, col2, col3, col4, col5 = st.columns([1,26,1,5,1])
   with col1:
     st.write("")
   with col2:
@@ -43,7 +63,10 @@ with st.container():
     </style> """, unsafe_allow_html=True)
   with col3:
     st.write("")
-  with col4:
+  with col4: 
+    image = Image.open(os.path.join(abs_path, 'logo.png'))
+    st.image(image)
+  with col5:
     st.write("")  
 
 html_line="""
@@ -107,17 +130,16 @@ html_card_footer3="""
 </div>
 """
 ### Block 0 #########################################################################################
-
 # Spécifications du dashboard
 # 1- Permettre de visualiser le score et l’interprétation de ce score pour chaque 
 #    client de façon intelligible pour une personne non experte en data science.
 # 2- Permettre de visualiser des informations descriptives relatives à un client
 #    (via un système de filtre).
 # 3- Permettre de comparer les informations descriptives relatives à un client à 
-#    l’ensemble des clients ou à un groupe de clients similaires.
+#    l’ensemble des clients.
 
 # URL de l'API
-api_adress = "https://hanen-p7-oc-ben.herokuapp.com/"
+api_adress = "https://hanen-p7-22-ben.herokuapp.com/"
 
 # Liste des IDs des clients
 @st.cache
@@ -131,7 +153,7 @@ def get_id_list():
 @st.cache
 def get_score(id):
     response = requests.get(api_adress + "get_score/?id=" + str(id))
-    content = json.loads(response.content)
+    content = json.loads(response.content.decode('utf-8'))
     score = content['score']
     proba = content['proba']
     thresh = content['thresh']
@@ -146,14 +168,15 @@ def get_information_descriptive(id):
     return data_client
 
 # Les informations descriptives relatives à l'ensemble de clients 
-
 @st.cache
 def get_data():
     response = requests.get(api_adress + "get_data/")
     content = json.loads(response.content)
-    X_tr_proc = pd.read_json(content['X'])
-    return X_tr_proc
-   
+    X_tr_proc = pd.read_json(content['df'])
+    y_tr = pd.read_json(content['y_train'])
+    return X_tr_proc, y_tr
+
+
 # Liste de feature importance 
 @st.cache
 def get_features_importances():
@@ -162,19 +185,14 @@ def get_features_importances():
     features_importances = pd.read_json(content['features_importances'], typ='series')
     return features_importances
 
-# Shap values
-@st.cache
-def get_shap_values(X_shap, y_shap):
-      # Prepare SHAP Values 
-    model_clf = lgb.LGBMClassifier().fit(X_shap, y_shap)
-    explainer = shap.TreeExplainer(model_clf)
-    shap_values = explainer.shap_values(X_shap)
-    expected_value = explainer.expected_value
-    # response = requests.get(api_adress + "get_shap_values/")
-    # content = json.loads(response.content)
-    # shap_values = np.array(content['shap_values'])
-    # expected_value = content['expected_value_json']
-    return (shap_values, expected_value)
+# # Shap values
+# @st.cache
+# def get_shap_values(X_shap, y_shap):
+#     # Prepare SHAP Values
+#     explainer = shap.TreeExplainer(model=model, model_output='raw')
+#     shap_values = explainer.shap_values(X_shap)
+#     expected_value = explainer.expected_value
+#     return (shap_values, expected_value)
 
 # Plot shap with streamlit 
 def st_shap(plot, height=None):
@@ -185,22 +203,37 @@ def st_shap(plot, height=None):
 
 
 
-
 ### Block 1#########################################################################################
+
 liste_id = get_id_list()
-data = get_data() 
+df, y_train = get_data() 
+X_shap = df.drop(columns=['SK_ID_CURR']).copy(deep=True)
+y_shap = y_train.drop(columns=['SK_ID_CURR']).copy(deep=True)
 
+# Load
+abs_path = os.path.dirname(os.path.realpath(__file__))
+path = os.path.join(abs_path, 'model', 'light_gbm_f2.sav')
+# with open(path, 'rb') as file:
+model_obj = pickle.load(open(path, 'rb'))
+model = model_obj
+thresh = 0.5
 
-df_sans_id = data.drop(columns=['identifiant'])
+df_sans_id = df.drop(columns=['SK_ID_CURR'])
 temp_lst = df_sans_id.columns.to_list()
 
 cat_features = df_sans_id.select_dtypes(exclude=[np.number]).columns.to_list()
 num_features = df_sans_id.select_dtypes(include=[np.number]).columns.to_list()
-# features_importances = get_features_importances()
-
+features_importances = get_features_importances()
+# shap_values, expected_value = get_shap_values(X_shap, y_shap)
+# explainer = shap.TreeExplainer(model=model, model_output='raw')
+# shap_values = explainer.shap_values(X_shap)
+# expected_value = explainer.expected_value
+shap_values = pickle.load(open("shap_values.sav (1)", 'rb')
+                          
+                               
 
 with st.expander("Mission du dashboard"):
-  st.write("Dashboard pour visualiser les informations sur un client demandant un \
+    st.write("Dashboard pour visualiser les informations sur un client demandant un \
             crédit bancaire et le comparer avec des profils similaires de client")
 
 
@@ -254,8 +287,6 @@ html_br="""
 <br>
 """
 st.markdown(html_br, unsafe_allow_html=True)
-
-
 html_card_header4="""
 <div class="card">
   <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #9C9B9B; padding-top: 5px; width: 850px;
@@ -264,41 +295,29 @@ html_card_header4="""
   </div>
 </div>
 """
-
 ### Block 2#########################################################################################
 with st.container():
     col1, col2, col3 = st.columns([1,42,1])
     with col1:
         st.write("")
-
     with col2:
         # Données des clients
           st.markdown(html_card_header4, unsafe_allow_html=True)
-
     with col3:
         st.write("")
-
-
 html_br="""
 <br>
 """
 st.markdown(html_br, unsafe_allow_html=True)
-
-
 if st.checkbox("Afficher les informations descriptives du client"):
   with st.container():
       col1, col2, col3 = st.columns([1,42,1])
       with col1:
           st.write("")
-
       with col2:
-          st.dataframe(df_client.set_index('identifiant'))
-
+          st.dataframe(df_client.set_index('SK_ID_CURR'))
       with col3:
           st.write("")
-
-
-
 html_card_header5="""
 <div class="card">
   <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #9C9B9B; padding-top: 5px; width: 850px;
@@ -312,47 +331,35 @@ with st.container():
     col1, col2, col3 = st.columns([1,42,1])
     with col1:
         st.write("")
-
     with col2:
         # Données des clients
           st.markdown(html_card_header5, unsafe_allow_html=True)
-
     with col3:
         st.write("")
-
-
 html_br="""
 <br>
 """
 st.markdown(html_br, unsafe_allow_html=True)
-
-
- ### Block 4 #########################################################################################
+  ### Block 4 #########################################################################################
 if st.checkbox("Afficher les informations descriptives de l'ensemble des clients"):
  
   with st.container():
       col1, col2, col3 = st.columns([1,42,1])
       with col1:
           st.write("")
-
       with col2:
           # Données des clients existant dans le jeu 
-          variable = st.checkbox("Customer's data")
-          # st.markdown(html_card_header15, unsafe_allow_html=True)
-          st.dataframe(data.set_index('identifiant'))
+            variable = st.checkbox("Customer's data")
+      
+            st.dataframe(df.set_index('SK_ID_CURR'))
            
-
       with col3:
           st.write("")
-
   html_br="""
   <br>
   """
   st.markdown(html_br, unsafe_allow_html=True)
-
-
   ### Block 5 #########################################################################################
-
   html_card_header6="""
   <div class="card">
     <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #9C9B9B; padding-top: 5px; width: 250px;
@@ -369,8 +376,43 @@ if st.checkbox("Afficher les informations descriptives de l'ensemble des clients
     </div>
   </div>
   """
+  
+  html_card_header7="""
+  <div class="card">
+    <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #9C9B9B; padding-top: 5px; width: 550px;
+    height: 50px;">
+      <h4 class="card-title" style="background-color:#9C9B9B; color:#F2EBEB; font-family:Georgia; text-align: center; padding: 10px 0;">Comparaison avec profils de clients similaires</h4>
+    </div>
+  </div>
+  """
 
+  html_card_footer7="""
+  <div class="card">
+    <div class="card-body" style="border-radius: 0px 0px 10px 10px; background: #9C9B9B; padding-top: 1rem;; width: 250px;
+    height: 50px;">
+      <p class="card-title" style="background-color:#9C9B9B; color:#F2EBEB; font-family:Georgia; text-align: center; padding: 0px 0;">Montly Value</p>
+    </div>
+  </div>
+  """
   ### Analyse Univariée ###
+  
+#   #### Mono & Bi analysis
+#     ### Dist Plot
+#   col1, col2 = st.beta_columns((2))
+  
+#   with col1:
+#         feature1 = st.selectbox('Choisissez la 1ère caractéristique:', df.index, index=0)
+#         valueCustomer1 = df.loc[df['SK_ID_]==user_input, feature1].values[0]
+#         fig = utils.plotDistOneFeature(dataRef, feature1, valueCustomer1)
+#         st.write(fig)
+        
+#    with col2:
+#         feature2 = st.selectbox('Choisissez la 2nd caractéristique:',df.index, index=1)
+#         valueCustomer2 = dataCustomer.loc[dataCustomer[loanColumn]==user_input, feature2].values[0]
+#         fig = utils.plotDistOneFeature(dataRef, feature2, valueCustomer2)
+#         st.write(fig)
+  
+  
   with st.container():
     col1, col2, col3, col4, col5 = st.columns([1,15,2,15,1])
     with col1:
@@ -380,7 +422,7 @@ if st.checkbox("Afficher les informations descriptives de l'ensemble des clients
       variable = st.selectbox ("Quel attribut voulez-vous analyser?", 
                                                 temp_lst,
                                                 )
-      fig = px.histogram(data,
+      fig = px.histogram(df,
                         x=variable,
                         title= 'Distribution de la variable : ' + variable,
                         )
@@ -390,16 +432,15 @@ if st.checkbox("Afficher les informations descriptives de l'ensemble des clients
                   line_dash="dash")
       fig.update_layout(width=600)
       st.plotly_chart(fig)
-
     with col3:
       st.write("")
     ### Radar plot ###
     with col4:
       st.markdown(html_card_header7, unsafe_allow_html=True)
-      columns_lst = data.columns.to_list()
+      columns_lst = df.columns.to_list()
       categories = st.multiselect("Sélectionnez les variables à comparer : ", 
-                                  options=data.columns.to_list(),
-                                  default= columns_lst[:5],
+                                  options=df.columns.to_list(),
+                                  default=columns_lst[:5],
                                   )
       # Choisir les 5 premieères variables sélectionées 
       if len(categories)<5:
@@ -410,8 +451,6 @@ if st.checkbox("Afficher les informations descriptives de l'ensemble des clients
       var_client_2 = df_client[categories[2]].mean()
       var_client_3 = df_client[categories[3]].mean()
       var_client_4 = df_client[categories[4]].mean()
-
-
       fig.add_trace(go.Scatterpolar(
             r=[var_client_0,
               var_client_1,
@@ -423,11 +462,11 @@ if st.checkbox("Afficher les informations descriptives de l'ensemble des clients
             name='Profil client'
       ))
       
-      var_data_0 = data[categories[0]].mean()
-      var_data_1 = data[categories[1]].mean()
-      var_data_2 = data[categories[2]].mean()
-      var_data_3 = data[categories[3]].mean()
-      var_data_4 = data[categories[4]].mean()
+      var_data_0 = df[categories[0]].mean()
+      var_data_1 = df[categories[1]].mean()
+      var_data_2 = df[categories[2]].mean()
+      var_data_3 = df[categories[3]].mean()
+      var_data_4 = df[categories[4]].mean()
       fig.add_trace(go.Scatterpolar(
             r=[var_data_0,
               var_data_1,
@@ -448,16 +487,12 @@ if st.checkbox("Afficher les informations descriptives de l'ensemble des clients
       )
       fig.update_layout(width=600)
       st.plotly_chart(fig)
-
     with col5:
       st.write("")
-
   html_br="""
   <br>
   """
   st.markdown(html_br, unsafe_allow_html=True)
-
-
 ### Block 6 #########################################################################################
 html_card_header8="""
 <div class="card">
@@ -471,13 +506,10 @@ with st.container():
     col1, col2, col3 = st.columns([1,42,1])
     with col1:
         st.write("")
-
     with col2:
             st.markdown(html_card_header8, unsafe_allow_html=True)
-
     with col3:
         st.write("")
-
 html_br="""
 <br>
 """
@@ -490,65 +522,121 @@ html_card_header9="""
   </div>
 </div>
 """
-
-
+html_card_header10="""
+<div class="card">
+  <div class="card-body" style="border-radius: 10px 10px 0px 0px; background: #9C9B9B; padding-top: 5px; width: 450px;
+  height: 50px;">
+    <h4 class="card-title" style="background-color:#9C9B9B; color:#F2EBEB; font-family:Georgia; text-align: center; padding: 10px 0;">Analyse SHAP</h4>
+  </div>
+</div>
+"""
 if st.checkbox("Afficher l'interprétation des résultats"):
   ### Block 7 #########################################################################################
   with st.container():
     col1, col2, col3 = st.columns([1,42,1])
     with col1:
         st.write("")
-
     with col2:
             st.markdown(html_card_header9, unsafe_allow_html=True)
-
     with col3:
         st.write("")
-
   html_br="""
   <br>
   """
   st.markdown(html_br, unsafe_allow_html=True)
   if st.checkbox("Afficher Importance de Variables"):
     with st.container():
-      col1, col3 = st.columns([1,32])
+      col1, col2, col3 = st.columns([1,32,1])
       with col1:
         st.write("")
-#       with col2:
-#         # features_importances
-#         features_importances_df = features_importances.reset_index()
-#         features_importances_df.columns = ['Feature', 'Top Features Importance']
-#         fig = px.bar(features_importances_df,
-#                     x='Top Features Importance',
-#                     y='Feature',
-#                     )
-#         fig.update_layout(height=900)
-#         st.plotly_chart(fig)
+      with col2:
+        # features_importances
+        features_importances_df = features_importances.reset_index()
+        features_importances_df.columns = ['Feature', 'Top Features Importance']
+        fig = px.bar(features_importances_df,
+                    x='Top Features Importance',
+                    y='Feature',
+                    )
+        fig.update_layout(height=900)
+        st.plotly_chart(fig)
       with col3:
         st.write("")
-
       html_br="""
       <br>
       """
       st.markdown(html_br, unsafe_allow_html=True)
-
-
   with st.container():
     col1, col2, col3 = st.columns([1,42,1])
     with col1:
         st.write("")
-
     with col2:
             st.markdown(html_card_header10, unsafe_allow_html=True)
-
     with col3:
         st.write("")
-
   html_br="""
   <br>
   """
   st.markdown(html_br, unsafe_allow_html=True)  
-  
+  if st.checkbox("Analyse SHAP"):
+    with st.container():
+      col1, col2, col3 = st.columns([1,32,1])
+      with col1:
+        st.write("")
+      with col2:
+        # Shap Values 
+        plot_type = st.selectbox('Veuillez choisir le plot SHAP à afficher', 
+                                   options=['Force Plot', 'Bar Plot', 'Dot Plot' ],)
+        if plot_type =='Bar Plot': 
+          fig, axes = plt.subplots(nrows=1,
+                  ncols=1,
+                  figsize=(6, 5),
+                  )        
+      
+          shap.summary_plot(shap_values,
+                            df.columns,
+                            plot_type ='bar',
+                            show = False, 
+                            )
+          axes = plt.gcf()
+          st.pyplot(fig, 
+                    bbox_inches='tight', 
+                    # dpi=300,
+                    # pad_inches=0,
+                    )
+        if plot_type =='Dot Plot':  
+          fig, axes = plt.subplots(nrows=1,
+              ncols=1,
+              figsize=(6, 5),
+              ) 
+          shap.summary_plot(shap_values,
+                            df.columns,
+                            show = False, 
+                            )
+          axes = plt.gcf() 
+          st.pyplot(fig, 
+                    bbox_inches='tight', 
+                    # dpi=300,
+                    # pad_inches=0,
+                    )
+        if plot_type =='Force Plot': 
+          index = df.loc[df['SK_ID_CURR']==selected_id,:].index[0]       
+          # visualize the client prediction's explanation 
+          shap.initjs()
+          st_shap(shap.force_plot(explainer.expected_value, 
+                                  shap_values[index,:],
+                                  df.drop(columns=['SK_ID_CURR']).iloc[index,:],
+                                  )
+                                  )
+          # visualize the training set predictions
+          # st_shap(shap.force_plot(explainer.expected_value, shap_values, X), 400)
+                    
+      with col3:
+        st.write("")
+      
+      html_br="""
+      <br>
+      """
+      st.markdown(html_br, unsafe_allow_html=True)
 html_line="""
 <br>
 <br>
