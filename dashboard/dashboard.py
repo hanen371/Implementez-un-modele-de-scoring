5471844a57564acc881c0553d1d1848be352057e
hanen371 <96344546+hanen371@users.noreply.github.com>
2022-04-20 06:59:42 +0200

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -591,7 +591,7 @@ if st.checkbox("Afficher l'interprétation des résultats"):
 #           shap.initjs()                
                    
         plot_type = st.selectbox('Veuillez choisir le plot SHAP à afficher', 
-                                   options=['Force Plot', 'Bar Plot', 'Dot Plot' ])
+                                   options=['Bar Plot', 'Dot Plot' ])
 
         if plot_type =='Bar Plot': 
           fig, axes = plt.subplots(nrows=1,
@@ -626,14 +626,7 @@ if st.checkbox("Afficher l'interprétation des résultats"):
                     # dpi=300,
                     # pad_inches=0,
                     )
-        if plot_type =='Force Plot': 
-          index = df.loc[df['SK_ID_CURR']==selected_id,:].index[0]       
-          # visualize the client prediction's explanation 
-          st_shap(shap.force_plot(expected_value, 
-                                  shap_values[index,:],
-                                  data.drop(columns=['SK_ID_CURR']).iloc[index,:]
-                                  )
-                                  )
+       
                     
       with col3:
         st.write("")
